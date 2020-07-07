050a273f63fba0726021a75edfacd4b5f939a281
Lucas Sales <lucassales398@gmail.com>
2020-07-07 01:18:18 -0300

    backend: tratando as msgs de erro

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycle');
+const errorHandler = require('../common/errorHandler');
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.after('post', errorHandler).after('put', errorHandler);
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
@@ -22,11 +24,11 @@ BillingCycle.route('summary', (req, res, next) => {
         $project: { _id: 0, credit: 1, debt: 1 }
     }], (error, result) => {
         if (error) {
-            res.status(500).json({ errors: [error] })
+            res.status(500).json({ errors: [error] });
         } else {
-            res.json(result[0] || { credit: 0, debt: 0 })
+            res.json(result[0] || { credit: 0, debt: 0 });
         }
-    })
+    });
 });
 
 module.exports = BillingCycle;
