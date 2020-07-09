54671cc2e550b0a9d8dd0f65e6a617bb3841646f
Lucas Sales <lucassales398@gmail.com>
2020-07-09 00:32:34 -0300

    frontend: refatorando tabActions

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -7,7 +7,7 @@ export function selectTab(tabId) {
 
 export function showTabs(...tabIds) {
     const tabsToShow = {}
-    tabIds.forEach(e => tabsToShow[e] = true)
+    tabIds.map(e => tabsToShow[e] = true)
     return {
         type: 'TAB_SHOWED',
         payload: tabsToShow
