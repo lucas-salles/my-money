650a4fcb4a15c02b461465c329542937c16b5e4a
Lucas Sales <lucassales398@gmail.com>
2020-07-09 21:37:22 -0300

    frontend: componente Input para CreditList

---
+++ frontend/src/common/form/input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default (props) => (
+  <input
+    {...props.input}
+    className="form-control"
+    type={props.type}
+    placeholder={props.placeholder}
+    readOnly={props.readOnly}
+  />
+);
